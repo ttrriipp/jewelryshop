cad86df0129de0690cb6efbf1786e563a3ab3d7b
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2024-12-05 15:02:41 +0800

    weqw

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class Main {public static void main(String[] args) {
+    System.out.println("Hello and welcome!");
 
-public class Main {
+    //TODO: what the fuck
+    for (int i = 1; i <= 5; i++) {
+        System.out.println("i = " + i);
+        }
+    }
 }
+
